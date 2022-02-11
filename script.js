bff03c3a98ee85c7a657d21230a2a1b0a408b2b0
Dan Grossberg <dwgrossberg@gmail.com>
2022-02-11 13:37:23 +0700

    Set up html and css framework, as well as essential js functions

---
+++ script.js
@@ -0,0 +1,22 @@
+//Basic math operator functions
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+//Operate function
+function operate(operator, a, b) {
+    return operator(a, b);
+}
+
