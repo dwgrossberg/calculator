3afb3b047812344e272d5257ebef549a7c927b7e
Dan Grossberg <dwgrossberg@gmail.com>
2022-02-16 08:02:46 +0700

    Revert makeNegative function to previous version

--- script.js
+++ script.js
@@ -161,18 +161,18 @@ function operatorSkip() {
 function makeNegative() {
     if (displayText.innerText === '') { //sets value to negative if nothing to evaluate
         displayText.innerText = '-'; 
-    //} else if (displayText.innerText === '÷' || displayText.innerText === 'x' || displayText.innerText === '-' || displayText.innerText === '+') {
-      //  return; //necessary to avoid accidentally inserting minus function into operates function displayValue[2]
+    } else if (displayText.innerText === '÷' || displayText.innerText === 'x' || displayText.innerText === '-' || displayText.innerText === '+') {
+        return; //necessary to avoid accidentally inserting minus function into operates function displayValue[2]
     } else if (displayValue.length > 0) { //modified preEquate and operatorPopulator to work with negative numbers
-        if (displayText.innerText = '-') {
+        //if (displayText.innerText = '-') {
         //displayText.innerText = displayText.innerText + 
         //in progress...
-
         displayValue[displayValue.length] = Number(displayText.innerText);
         operates();
-        displayValue[displayValue.length] = subtracts;
-        displayValue[displayValue.length] = Number(displayText.innerText);
-        displayText.innerText = '-';     
     }
-}
+    displayValue[displayValue.length] = subtracts;
+    displayValue[displayValue.length] = Number(displayText.innerText);
+    displayText.innerText = '-';     
+
+    
 }
